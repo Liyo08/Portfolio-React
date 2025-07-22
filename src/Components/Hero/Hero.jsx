30cf380c55895cf63345ee56658a159e9f5181d5
Liyo08 <133187882+Liyo08@users.noreply.github.com>
2025-07-22 20:12:36 +0530

    hero section completed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import './Hero.css'
 
 const Hero = () => {
-  return (
-    <div>
-
-    </div>
-  )
+    return (
+        <div className='hero'>
+            <img src="" alt="" />
+            <h1><span>Hi, I'm Liyo Tom,</span> Frontend Developer</h1>
+            <p>I'm a frontend developer with a passion for creating beautiful and functional web applications. I love to learn new technologies and improve my skills.</p>
+            <div className="hero-action">
+                <div className="hero-connect">Connect with Me</div>
+                <div className="hero-resume">My Resume</div>
+            </div>
+        </div>
+    )
 }
 
 export default Hero
