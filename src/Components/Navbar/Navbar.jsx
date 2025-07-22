cc05eb239e59c6e87261a45b168abf5f7203b157
Liyo08 <133187882+Liyo08@users.noreply.github.com>
2025-07-22 18:49:32 +0530

    navbar design completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-<img src="" alt="" />
-    </div>
-  )
+    return (
+        <div className='navbar'>
+            <img src="" alt="" />
+            <ul className="navmenu">
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Services</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+
+            </ul>
+            <div className="nav-connect">Connect with Me</div>
+        </div>
+    )
 }
 
 export default Navbar
