ba9228a504068791c445b5bee2871b111cb39f85
Liyo08 <133187882+Liyo08@users.noreply.github.com>
2025-07-23 01:16:01 +0530

    project design completed

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Project.css'
+import Project_data from '../../assets/project_data';
+
 
 const Project = () => {
   return (
     <div className='project'>
-        
+        <div className="project-title">
+            <h1>My Projects</h1>
+            <img src="" alt="" />
+        </div>
+        <div className="project-container">
+{Project_data.map((project,index)=>{
+    return <div key={index} className="project-format">
+       
+        <h2>{project.s_name}</h2>
+        <p>{project.s_desc}</p>
+        <div className="project-readmore">
+            <p>Read more</p>
+        </div>
+    </div>
+})}
+        </div>
 
     </div>
   )
---
+++ src/assets/project_data.js
@@ -0,0 +1,34 @@
+const Project_data=[
+{
+    s_no:"1",
+    s_name:"Zenalign",
+    s_desc:": A smart companion website to detect and perfect poses ",
+},
+{
+    s_no:"2",
+    s_name:"Agriguru",
+    s_desc:"Crop recommendation system using ML based on soil, season & location",
+},
+{
+    s_no:"3",
+    s_name:"AlignMe",
+    s_desc:"AI-Powered Lifestyle Assistant App for Yoga Wellness and Correction",
+},
+{
+    s_no:"4",
+    s_name:"Collage Smart Entry",
+    s_desc:"Creating a Digital Visitor Entry System to improve admin response time by 45%",
+},
+{
+    s_no:"5",
+    s_name:"Dromed: Healing fast",
+    s_desc:"Delivers medicines swiftly and safely using drones, ensuring timely healthcare access anywhere",
+},
+{
+    s_no:"6",
+    s_name:"Student currency: ",
+    s_desc:"A special currency for students that parents can manage, and learning financial discipline",
+}
+
+]
+export default Project_data;
