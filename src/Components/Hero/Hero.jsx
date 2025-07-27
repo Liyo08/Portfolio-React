033b2a6443d3cee63faef85e46a0f1597b3151a6
Liyo08 <133187882+Liyo08@users.noreply.github.com>
2025-07-27 21:31:17 +0530

    Resume added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -10,7 +10,9 @@ const Hero = () => {
         <p>I'm a frontend developer with a passion for creating beautiful and functional web applications. I love to learn new technologies and improve my skills.</p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchorlink' offset={50} href='#contact'>Connect with Me</AnchorLink></div>
-        <div className="hero-resume">My Resume</div>
+        <a href="https://drive.google.com/file/d/1RDuDtwju5IMi5JakOPCCoiFR8zPRX4pP/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+  <div className="hero-resume">My Resume</div>
+</a>
 </div>
     </div>
   )
