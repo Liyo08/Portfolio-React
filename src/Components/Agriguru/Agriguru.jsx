4bded27b9502fe3c67d6e8e5fedbd18808ffc3e8
Liyo08 <133187882+Liyo08@users.noreply.github.com>
2025-07-31 22:15:12 +0530

    Router installed and created agriguru page

---
+++ src/Components/Agriguru/Agriguru.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Agriguru = () => {
+    return (
+        <div className='agriguru'>
+            <h1><span>Agriguru</span> Crop recommendation system using ML based on soil, season & location</h1>
+            <p>Crop planning guidance, delivering market insights to over 50 farmers, chatbot assistance and weather
+monitoring for increasing yield prediction accuracy by 35%.
+Tools used: HTML, CSS, JS, ReactJS, Bootstrap, Flask</p>
+
+        </div>
+    )
+}
+
+export default Agriguru
