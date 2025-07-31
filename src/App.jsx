1a78155690ac6874848f9ff4fb874868ff45b51a
Liyo08 <133187882+Liyo08@users.noreply.github.com>
2025-07-31 23:14:45 +0530

    Navbar removed from details pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,53 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-
+import { useLocation, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Project from './Components/Project/Project'
-import Certificate from './Components/Certificate/Certificate'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
-
 import Agriguru from './Components/Agriguru/Agriguru'
-import AlignMe from './Components/AlignMe/AlignMe'
-import Currency from './Components/Currency/Currency'
-import Dromed from './Components/Dromed/Dromed'
-import Smart from './Components/Smart/Smart'
 import Zenalign from './Components/Zenalign/Zenalign'
+import AlignMe from './Components/AlignMe/AlignMe'
+import Dromed from './Components/Dromed/Dromed'
+import Currency from './Components/Currency/Currency'
+import Smart from './Components/Smart/Smart'
 
 const App = () => {
+  const location = useLocation()
+
+  // Routes where you do NOT want the navbar/footer
+  const hideLayoutRoutes = [
+    '/projects/agriguru',
+    '/projects/zenalign',
+    '/projects/alignme',
+    '/projects/dromed',
+    '/projects/smart',
+    '/projects/currency'
+  ]
+
+  const shouldHideLayout = hideLayoutRoutes.includes(location.pathname.toLowerCase())
+
   return (
     <>
-      <Navbar />
+      {!shouldHideLayout && <Navbar />}
 
-      {/* Routes only handle switching between main pages */}
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <About />
-              <Project />
-              <Contact />
-              <Footer />
-            </>
-          }
-        />
+        <Route path="/" element={
+          <>
+            <Hero />
+            <About />
+            <Project />
+            <Contact />
+            <Footer />
+          </>
+        } />
         <Route path="/projects/agriguru" element={<Agriguru />} />
+        <Route path="/projects/zenalign" element={<Zenalign />} />
         <Route path="/projects/alignme" element={<AlignMe />} />
-        <Route path="/projects/currency" element={<Currency />} />
         <Route path="/projects/dromed" element={<Dromed />} />
         <Route path="/projects/smart" element={<Smart />} />
-        <Route path="/projects/zenalign" element={<Zenalign />} />
+        <Route path="/projects/currency" element={<Currency />} />
       </Routes>
     </>
   )
