2f9913c0f8e1f961d9f814b27db93adcbd11a00f
Liyo08 <133187882+Liyo08@users.noreply.github.com>
2025-08-03 12:11:49 +0530

    Footer section made responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,37 +1,39 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
         <div className='footer' id='footer'>
             <div className="footer-title">
                 <h1>Subscribe.</h1>
-                </div>
+            </div>
+
             <div className="footer-top">
                 <div className="footer-top-left">
-                    <img src="" alt="" />
-                    <p>I am a frontend developer passionate about transforming ideas into polished digital experiences<br></br> Ernakulam, Kerala</p>
+                    <img src="" alt="Logo or illustration" />
+                    <p>I am a frontend developer passionate about transforming ideas into polished digital experiences.<br />Ernakulam, Kerala</p>
                 </div>
+
                 <div className="footer-top-right">
                     <div className="footer-email-input">
-                        <input type="email" placeholder='Enter your email' />
+                        <input type="email" placeholder='Enter your email' aria-label="Email input" />
                     </div>
-                    <div className="footer-subscribe">
-                        Subscribe
-                    </div>
+                    <button className="footer-subscribe">Subscribe</button>
                 </div>
             </div>
+
             <hr />
+
             <div className="footer-bottom">
                 <p className='footer-bottom-left'>© 2025 Liyo Tom. All rights reserved.</p>
                 <div className="footer-bottom-right">
-                    <p>Terms of Services</p>
+                    <p>Terms of Service</p>
                     <p>Privacy Policy</p>
                     <p>Connect with me</p>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
